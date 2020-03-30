7deebce21c2bee8550a60e76eebc7041a5ae7191
micahang0127 <psa5013@hanmail.net>
2020-03-31 05:07:29 +0900

    commit

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const cookieParser = require('cookie-parser');
+// const expressSession = require('express-session');
+// const passport = require('passport');               // 에러) Unkown authentication strategy "local"   
+// // require('./passport/index')(passport);           // -> 위 에러 해결방안.
+// const dotenv = require('dotenv');                   // 해야 session secet을 인식해 값을 넣을 수 있다
+// const flash = require('connect-flash');             // 에러 메시지를 redirect 하는 과정에서 쉽게 전달해주는 모듈
+// const user = require('./api/userApi');
+// const sales = require('./api/salesApi');
+// const rank = require('./api/rankApi');
+// const store = require('./api/storeApi');
 
-// Serve static files from the React app
+
+
+
+
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
@@ -13,11 +27,12 @@ app.get('/api/test', (req, res) => {
 
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port);
+app.listen(port , () => console.log(`port는 ${port}`));
+
+
+
